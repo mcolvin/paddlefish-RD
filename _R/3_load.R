dde0c35634267cba78fae9514b0320c4ae7b26fb
mcolvin <colvin.mike@gmail.com>
2017-07-27 17:16:41 -0500

    waiting on acoustic data to be fixed

--- _R/3_load.R
+++ _R/3_load.R
@@ -32,10 +32,14 @@ effort$year<-as.numeric(format(effort$date, "%Y"))
 effort<- subset(effort,set_number!=9999)
 ## SUBSET OCCASIONS PRIOR TO
 ## MARCH 2016 (when we put in reciever
-effort<- subset(effort, !(year==2016 & doy < 78| year==2015))
+#effort<- subset(effort, !(year==2016 & doy < 78| year==2015))
 
-
-
+## SUMMARIZE PRIMARY OCCASIONS
+primary_occasions<- data.frame(
+    occasion=unique(paste(effort$year,effort$doy,sep="_")))
+primary_occasions$id<- 1:nrow(primary_occasions)
+primary_occasions$year_f<- as.numeric(substr(primary_occasions$occasion,1,4))+
+    (as.numeric(substr(primary_occasions$occasion,6,nchar(as.character(primary_occasions$occasion))))/365)
 #######################################################################    
 # TAGGING DATA
 #######################################################################
@@ -45,13 +49,13 @@ taggingData<- sqlFetch(comm,
 
 ## EXTRACT DAY OF YEAR FROM DATE
 taggingData$doy<-as.numeric(
-    format(taggingData$date, "%j"))
+    format(taggingData$Date, "%j"))
 ## EXTRACT YEAR FROM DATE
 taggingData$year<-as.numeric(
-    format(taggingData$date, "%Y"))
+    format(taggingData$Date, "%Y"))
 ## SUBSET OCCASIONS PRIOR TO
 ## MARCH 2016 (when we put in reciever
-taggingData<- subset(taggingData, !(year==2016 & doy < 78| year==2015))
+#taggingData<- subset(taggingData, !(year==2016 & doy < 78| year==2015))
 
 
 
@@ -65,6 +69,14 @@ poolReciever<- sqlFetch(comm,
 ## DATA FIX TRANSMITTER CODE
 ## FIX NON-UNIFORM TRANSMITTER CODES
 poolReciever$date<-as.POSIXct(poolReciever$date)
+
+## KEEP ALL DATA TO CHECK DAILY IN/OUT
+poolReciever_all<-poolReciever
+poolReciever_all$doy<- as.numeric(
+    format(poolReciever_all$date, "%j"))
+poolReciever_all$year<- as.numeric(
+    format(poolReciever_all$date, "%Y"))
+ poolReciever_all<-poolReciever_all[order(poolReciever_all$date),]   
 ## SUBSET OUT DAYS THAT MATCH EFFORT OCCASIONS
 poolReciever<- subset(poolReciever, date %in% unique(effort$date))
 
@@ -74,10 +86,10 @@ poolReciever$year<- as.numeric(
     format(poolReciever$date, "%Y"))
 poolReciever$set<- 0  # FOR SETTING UP FIRST OCCASION
 ## SUBSET OCCASIONS PRIOR TO
-## MARCH 2016 (when we put in reciever
-poolReciever<- subset(poolReciever, !(year==2016 & doy < 78| year==2015))
+## MARCH 2016 (when we put in receiver
+#poolReciever<- subset(poolReciever, !(year==2016 & doy < 78| year==2015))
 
-
+poolReciever<-poolReciever[order(poolReciever$date),]
     
     
 
@@ -89,8 +101,16 @@ poolReciever<- subset(poolReciever, !(year==2016 & doy < 78| year==2015))
 
 tags<- sqlFetch(comm,
     'qry-transmitter-number')
+tags$first_tagged<- paste(
+    format(tags$implantDate,"%Y"),
+    as.numeric(format(tags$implantDate,"%j")),
+    sep="_")
+tags<-merge(tags,primary_occasions  
+    ,by.x="first_tagged",
+    by.y="occasion",all.x=TRUE)
 ## ADD A PIT TAG NUMBER TO EACH TRANSMITTER
-pit<- dcast(taggingData,pit+transmitter~"yy",value.var="transmitter",length)[,-3]
+pit<- dcast(taggingData,pit+transmitter~"yy",
+    value.var="transmitter",length)[,-3]
 pit<- subset(pit, !(is.na(transmitter )))
 tags<- merge(tags,pit,by="transmitter")
 
@@ -101,19 +121,20 @@ tags<- merge(tags,pit,by="transmitter")
 # SET UP DATA FOR ROBUST DESIGN 
 # IN PROGRAM MARK
 #######################################################################
-poolReciever<- merge(poolReciever,tags[,c(1,4)],by="transmitter")
-
+poolReciever<- merge(poolReciever,tags[,c(1,6)],by="transmitter")
+poolReciever_all<- merge(poolReciever_all,tags[,c(1,6)],by="transmitter")
 xx<- rbind.fill(taggingData,poolReciever)
 xx$tmp<- 1 # DUMMY FOR SUMMARY
 
 
 ## OCCASIONS VECTOR FOR RMARK
+## RECIEVER OCCASIONS ARE iNCLUDED
 occasions<- dcast(effort, 
     year+doy~"sets",
     value.var="set_number",
     fun=length)
 occ<- unlist(lapply(1:nrow(occasions),
-    function(x){c(rep(0,occasions$sets[x]),1)}))
+    function(x){c(rep(0,occasions$sets[x]),1)}))# ADD 1 TO THE END TO ACCOUNT FOR ACOUSTIC
 occ<- occ[-length(occ)]# drop last 1 for Rmark format
 all_occasions<- unlist(lapply(1:nrow(occasions),
     function(x){
@@ -123,7 +144,13 @@ all_occasions<- unlist(lapply(1:nrow(occasions),
             sep="_")
     }))
 
-        
+## RECIEVER OCCASIONS ARE iNCLUDED    
+
+occ_noac<- unlist(lapply(1:nrow(occasions),
+    function(x){c(rep(0,occasions$sets[x]-1),1)}))# ADD 1 TO THE END TO ACCOUNT FOR ACOUSTIC
+occ_noac<- occ_noac[-length(occ_noac)]# drop last 1 for Rmark format
+
+          
         
 ## THE BIG MAMA JAMMA - THE WHOLE ENCHILADA
 names(occasions)[3]<- "set"
@@ -138,6 +165,7 @@ xxx$occ_id<- factor(paste(xxx$year,xxx$doy,xxx$set,sep="_"),
 xxx<- xxx[!(is.na(xxx$occ_id)),]
 xxx<- xxx[!(is.na(xxx$tmp)),]
 
+
 ch<- dcast(xxx, pit~occ_id,
     value.var='tmp',
     drop=FALSE,
@@ -150,16 +178,18 @@ ch<-ch[which(zeros==1),]
 ## FISH LEVEL COVARIATES
 fish<- data.frame(pit=ch[,1])
 fish<- merge(fish,tags,by='pit',all.x=TRUE)
-fish$acoustic<- as.factor(ifelse(is.na(fish$transmitter)==TRUE,"No","Yes"))
-
+fish$id<- ifelse(is.na(fish$id),0,fish$id)
 ## MAKE SURE EVERYBODY MATCHES UP
 ch<- ch[order(ch$pit),]
 fish<- fish[order(fish$pit),]
-
+ch_raw<-ch
 # BUNLDLE THIS UP 
 ch<- data.frame(ch=apply(ch[,-1],1,paste,collapse=""),
     freq=1,
-    acoustic=fish$acoustic,
+    first_captured=fish$id,
     stringsAsFactors=FALSE)
  
-
+ch_noac<- data.frame(ch=apply(ch_raw[,-c(1,grep("_0",names(ch_raw)))],1,paste,collapse=""),
+    freq=1,
+    first_captured=fish$id,
+    stringsAsFactors=FALSE)
--- _R/7_analysis.R
+++ _R/7_analysis.R
@@ -1,184 +1,127 @@
 
-input<-list()
-input$nprim = 10
-input$phi=rep(0.8,input$nprim-1)## SURVIVAL
-# FECUNDITY
-input$f=rep(0,input$nprim-1)
-input$n=500 # initial population size
-## RANDOM MOVEMENT PRIMARY
-input$gam_d_prime=rep(0.3,input$nprim-1) # OBSERVABLE[t-1] --> UNOBSERVABLE[t]; TEMP EMIGRATION
-input$nsec=rep(4,input$nprim)## SECONDARY OCCASIONS
-## RANDOM MOVEMENT SECONDARY
-input$gam_d_prime2<-rep(0,input$nprim) # OBSERVABLE[t-1] --> UNOBSERVABLE[t]
-input$p=rep(0.3,input$nprim)## CAPTURE PROBABILITY
-inputs<-input
-ch<-sim_ch(inputs=input)
-
-
-library(RMark)
-rd<-process.data(data=ch$ch, 
+rd<- process.data(data=ch, 
+    groups=c("first_captured"),# 
     model="Robust", 
-    time.intervals=ch$occs)
-S=list(formula=~1)# SURVIVAL
-# SHARE = TRUE TO SET C = P
-p=list(formula=~1,share=TRUE)# CAPTURE PROBABILITY
-f0<- list(formula=~time) # NUMBER NOT ENCOUNTERED
-GammaDoublePrime=list(formula=~1,share=TRUE)
-GammaPrime=list(formula=~1)
-fit<-mark(data = rd, 
-    model = "Robust", 
-    time.intervals=time.intervals,
-    filename="rd",
-    model.parameters=list(
-        S=S,
-        GammaDoublePrime=GammaDoublePrime,
-        # GammaPrime=GammaPrime, # not needed when share=TRUE
-        p=p),
-    threads=2,
-    brief=TRUE)
-summary(fit)
-            
+    time.intervals=occ) 
+rd_ddl<-make.design.data(rd)
 
-T2 <- input$nsec # number of secondary periods per primary period
-T <- length(T2) # number of primary periods
-ch_array<-array(0,c(nrow(ch$ch_mat),max(T2),T))
-secs<-cbind(cumsum(T2),(cumsum(T2)-T2)+1)
-for(i in 1:T)
+# PLACES TO SET TO 1
+## ACOUSTICALLY TAGGED FISH IN SESSION 1
+indx1<-c()
+indx0<-c()
+for(i in 1:max(ch$first_captured))
     {
-    ch_array[,,i]<- ch$ch_mat[,secs[i,1]:secs[i,2]]
+    if(i <5)# receiver went in on occasion 5
+        {
+        indx1<-c(indx1,which(rd_ddl$p$first_capture==i & 
+            rd_ddl$p$session %in% c(5:max(primary_occasions$id)) & 
+            rd_ddl$p$time==1))         
+        }
+    if(i>=5)# fish going in after the reciever was put in
+        {
+         indx1<-c(indx1,which(rd_ddl$p$first_capture==i & 
+            rd_ddl$p$session %in% c((i+1):max(primary_occasions$id)) & 
+            rd_ddl$p$time==1))       
+        }
+    }  
+
+for(i in 5:max(ch$first_captured))
+    {    
+    indx0<-c(indx0, which(rd_ddl$p$first_capture==i & 
+        rd_ddl$p$session %in% c(1:i) & 
+        rd_ddl$p$time==1))
     }
+## BACKFILL 0S WHERE RECIEVER WAS NOT OUT
+indx0<-c(indx0, 
+    which(rd_ddl$p$first_capture%in%c(0:4) & 
+        rd_ddl$p$session %in% c(1:4) & 
+        rd_ddl$p$time==1))
+indx0<-c(indx0, 
+    which(rd_ddl$p$first_capture==0 & rd_ddl$p$time==1))
+table(indx1%in%indx0)
+table(indx0%in%indx1)
 
+## BREAK APART WHETHER FISH ARE PIT ONLY OR NOT
+rd_ddl$p$pbin<-0
+rd_ddl$c$pbin<-0
+rd_ddl$p$pbin[which(rd_ddl$p$time==1)]<-1
 
-N<- nrow(ch$ch_mat)
-n_aug<- N # doubl up population
-y<- array(NA,c(N+n_aug, max(T2),T))
-y[1:N,,]<-ch_array
-y[(N+1):nrow(y),,]<-0# data augmentation
-z<- matrix(1,nrow(y),T)
-z[1:N,]<-4
-dat<-list(
-    M = nrow(y),# number of individuals
-    T = length(T2),
-    T2 = input$nsec,# vector of number of secondary occasions
-    y = y) # array of capture histories [ind, sec occasion, primary occasion]
-  
-
-## initial values
-## set for each chain
-inits<- function()
-	{
-	list(lo_omega=rnorm(1,0,1),lo_gamma=rnorm(1,0,1),lo_phi=rnorm(1,0,0.37),
-        lo_p=rnorm(1,0,0.37),z=z)
-	}
-	
-
-    
-## WHAT PARAMETERS TO KEEP TRACK OF DURING ESTIMATION
-params<-c("omega","p","S","lambda","gammaPrime")	
-
-# THIS WILL ONLY RUN IF YOU HAVE JAGS INSTALLED 
-# AND THE R2jags PACKAGE
-out <- jags(data=dat,
-	inits=inits,
-	parameters=params,	
-	model.file=mod_RD,
-	n.chains = 3,	
-	n.iter = 1500,	
-	n.burnin = 600, 
-	n.thin=2,
-	working.directory=getwd())
-out  # OUTPUT
-out$BUGSoutput$mean$N # ESTIMATED NUMBER OF FISH IN POOL
-traceplot(out)# LOOK AT TRACEPLOTS FOR CONVERGENCE.
-	
-	  
-
-
-  
-m1 <- jags.model(file=modname1,
-    data=dat,
-    inits=inits,
-    n.chains=nchains,
-    n.adapt=nadapt) 
-
-
-
-     
-     
-     
-     
-     
-   
-# need to add date of tagging     
-rd_aux<-process.data(data=ch, 
-    groups="acoustic",
-    model="Robust", 
-    time.intervals=occ)
-rd_ddl<-make.design.data(rd_aux)
-
-
-
-## SURVIVAL
-### TIME ALLOWS HETERGENEITY AMONG
-### PRIMARY OCCASIONS
-S=list(formula=~time)# SURVIVAL
-
-
-# NUMBER NOT ENCOUNTERED
-f0<- list(formula=~time) 
-
-
-## MIGRATION
-### WE'LL DO RANDOM FOR NOW
-### AND FIX PERIODS WHERE STREAM
-### IS TOO LOW TO 0?
-
-## RANDOM TE
-GammaDoublePrime=list(formula=~1,share=TRUE,fixed=0)
-    
-    
-    
-## CAPTURE PROBABILITY
-### SHARE = TRUE TO SET C = P
-# FIX P TO 1 FOR ACOUSTIC FISH AND 0 FOR PIT TAGS)
-p_fixed1_indx<- c(which(rd_ddl$p$acoustic=="Yes" &  
-    rd_ddl$p$Time==0))
-p_fixed0_indx<- c(which(rd_ddl$p$acoustic=="No" & 
-        rd_ddl$p$Time==0))
-
-### SET UP P
-p=list(formula=~time:session,share=TRUE,
+## SET VALUES TO BE CLOSE TO 1 OR 0 AS IT MESSES UP 
+## THE ESTIMATES OF S AND GAMMA PRIME
+p=list(formula=~pbin:first_captured+session,share=TRUE,
     fixed= list(
-        index = c(p_fixed1_indx, p_fixed0_indx), 
-        value=  c(rep(1,length(p_fixed1_indx)),
-            rep(0, length(p_fixed0_indx))))) 
-
-
-
-
-
-
-
-fit<-mark(data = rd_aux, 
+        index =c(indx1,indx0), 
+        value= c(rep(0.999,length(indx1)),rep(0.001,length(indx0)))))
+S=list(formula=~1)# SURVIVAL
+GammaDoublePrime=list(formula=~1,share=TRUE,fixed=0)
+#p=list(formula=~1)# SURVIVAL
+fit_ac<-mark(data = rd,
+    ddl=rd_ddl,
     model = "Robust", 
     time.intervals=time.intervals,
     model.parameters=list(
         S=S,
         GammaDoublePrime=GammaDoublePrime,
-        # GammaPrime=GammaPrime, # not needed when share=TRUE
         p=p),
     threads=2,
-    brief=TRUE)
-derived<- fit$results$derived$`N Population Size`
+    brief=FALSE,output=FALSE,invisible=TRUE)
+N_ac<- fit_ac$results$derived$`N Population Size`
+NN_ac<- (matrix(N_ac$estimate,nrow=40,byrow=FALSE))
+NN_ac<- rowSums(NN_ac)
+# by adding a group it give separate estimates for f0
+plot(NN_ac~primary_occasions$year_f,ylim=c(0,50),type='b')
+ff_ac<-summary(fit_ac,show.fixed=TRUE)$reals$p
+SS_ac<-summary(fit_ac,show.fixed=TRUE)$reals$S
+gdp_ac<-summary(fit_ac,show.fixed=TRUE)$reals$GammaDoublePrime
 
-summary(fit)
+vals<-do.call("rbind.data.frame",ff_ac)
 
-plot(derived$estimate)
-
-
+summary(fit_ac)$beta
 
 
 
 
+ch_raw[c(55,77,79,81,85,94),grep("_0",names(ch_raw))]
+ch_raw[c(55,77,79,81,85,94),1]
+# pit: 1005085863   1005085888   1005085891   1005085894   1005085898 121021201081
+
+2016_104_0 
+2016_113_0
+2016_295_0
+2016_322_0
+
+poolReciever_all[poolReciever_all$pit=="1005085863",]
+poolReciever_all[poolReciever_all$pit=="121021201081",]
+poolReciever_all[poolReciever_all$pit=="1005085898",]
+poolReciever_all[poolReciever_all$pit=="1005085894",]
+poolReciever_all[poolReciever_all$pit=="1005085888",]
+poolReciever_all[poolReciever_all$pit=="1005085891",]
+poolReciever_all[poolReciever_all$pit%in%c("1005085863","1005085891"),]
 
 
+rd<- process.data(data=ch_noac, 
+    #groups=c("first_captured"), 
+    model="Robust", 
+    time.intervals=occ_noac) 
+rd_ddl<-make.design.data(rd)
+S=list(formula=~1)# SURVIVAL
+GammaDoublePrime=list(formula=~1,share=TRUE,fixed=0)
+p=list(formula=~1,share=TRUE)# SURVIVAL
+fit_ac<-mark(data = rd,
+    ddl=rd_ddl,
+    model = "Robust", 
+    time.intervals=time.intervals,
+    model.parameters=list(
+        S=S,
+        GammaDoublePrime=GammaDoublePrime,
+        p=p),
+    threads=2,
+    brief=FALSE,output=FALSE,invisible=TRUE)
+N_ac<- fit_ac$results$derived$`N Population Size`
+NN_ac<- (matrix(N_ac$estimate,nrow=40,byrow=FALSE))
+NN_ac<- rowSums(NN_ac)
+# by adding a group it give separate estimates for f0
+matplot(NN_ac,ylim=c(0,50))
+ff_ac<-summary(fit_ac,show.fixed=TRUE)$reals$p
+SS_ac<-summary(fit_ac,show.fixed=TRUE)$reals$S
+gdp_ac<-summary(fit_ac,show.fixed=TRUE)$reals$GammaDoublePrime
