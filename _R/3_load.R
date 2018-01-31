cff939508870c442c0d08e33b6b69da31eb2c175
mcolvin <colvin.mike@gmail.com>
2018-01-31 14:38:31 -0600

    setting up data for multiple M0

--- _R/3_load.R
+++ _R/3_load.R
@@ -15,7 +15,8 @@ dat<-list()
 #
 #######################################################################
 
-comm<- odbcConnectAccess2007(file.path(Sys.getenv("USERPROFILE"),"Google Drive/Paddlefish/Paddlefish Database.accdb"))
+#comm<- odbcConnectAccess2007(file.path(Sys.getenv("USERPROFILE"),"Google Drive/Paddlefish/Paddlefish Database.accdb"))
+comm<- odbcConnectAccess2007("C:/Users/mcolvin/Documents/projects/Paddlefish/analysis/data/Paddlefish Database.accdb")
 
 #######################################################################
 #
@@ -35,14 +36,19 @@ effort<- sqlQuery(comm,
 effort$doy<-as.numeric(format(effort$date, "%j"))
 effort$year<-as.numeric(format(effort$date, "%Y"))
 
+## FORMATTING INDICES
+effort<- effort[order(effort$date,effort$set_number),]
+effort$id<-c(1:nrow(effort))
+effort$occasionid<- sequence(rle(effort$occasionId)$length)  
 
-primary<- aggregate(effort~occasionId+date+doy,effort,length)
-primary<- primary[order(primary$occasionId),]
-primary$dt<- c(0,round(diff(primary$date),0))
-primary<- primary[,-2]
-names(primary)[3]<- "secoccs"
-dat$primary<- primary
-dat$secondary<- effort[,-c(3,6,7,8)]
+
+#primary<- aggregate(effort~occasionId+date+doy,effort,length)
+#primary<- primary[order(primary$occasionId),]
+#primary$dt<- c(0,round(diff(primary$date),0))
+#primary<- primary[,-2]
+#names(primary)[3]<- "secoccs"
+#dat$primary<- primary
+#dat$secondary<- effort[,-c(3,6,7,8)]
 
 
 ####################################################################### 
@@ -50,37 +56,22 @@ dat$secondary<- effort[,-c(3,6,7,8)]
 ## TAGGING DATA
 ##
 #######################################################################
-taggingData<- sqlQuery(comm,"SELECT [Paddlefish Tagging Data].ID AS id,
-        [Paddlefish Tagging Data].occasionId AS occasionId,
-        [Paddlefish Tagging Data].Date,
-        [Paddlefish Tagging Data].set AS set_number,         
-        [Paddlefish Tagging Data].tl, 
-        [Paddlefish Tagging Data].rfl,         
-        [Paddlefish Tagging Data].efl, 
-        [Paddlefish Tagging Data].girth,         
-        [Paddlefish Tagging Data].floy_color, 
-        [Paddlefish Tagging Data].floy_number,         
-        [Paddlefish Tagging Data].weight, 
-        [Paddlefish Tagging Data].pit,         
-        [Paddlefish Tagging Data].new_recap, 
-        [Paddlefish Tagging Data].transmitter,         
-        [Paddlefish Tagging Data].sex     
-    FROM [Paddlefish Tagging Data]     
-    ORDER BY [Paddlefish Tagging Data].Date;")
+taggingData<- sqlQuery(comm,"SELECT [Paddlefish Tagging Data].ID, [Paddlefish Tagging Data].date, [Paddlefish Tagging Data].set AS set_number, [Paddlefish Tagging Data].tl, [Paddlefish Tagging Data].rfl, [Paddlefish Tagging Data].efl, [Paddlefish Tagging Data].girth, [Paddlefish Tagging Data].floy_color, [Paddlefish Tagging Data].floy_number, [Paddlefish Tagging Data].weight, [Paddlefish Tagging Data].pit, [Paddlefish Tagging Data].new_recap, [Paddlefish Tagging Data].transmitter, [Paddlefish Tagging Data].sex
+FROM [Paddlefish Tagging Data];")
 
-taggingData<- taggingData[which(is.na(taggingData$occasionId)==FALSE),]
-    
-## MERGE SECONDARY ID
-xx<-merge(taggingData,dat$secondary, by=c("occasionId","set_number"),all.x=TRUE)
-xx$tmp<-1
-xx$secid<-factor(xx$id.y,levels=dat$secondary$id)
 
+## ASSIGN OCCASSION ID 
+
+tmp<-merge(taggingData,effort[,c(1:4)],by=c("date","set_number"),all.x=TRUE)
+tmp<- tmp[which(is.na(tmp$occasionId)==FALSE),]
+   
 
 ## DETERMINE WHICH FISH ARE ACOUSTICALLY TAGGED
-acc_tags<- reshape2::dcast(taggingData,pit~transmitter,value.var='id',length)
+acc_tags<- reshape2::dcast(tmp,pit~transmitter,value.var='id',length)
 acc_tags<- acc_tags[,-which(names(acc_tags)=="NA")]  
 acc_tags<- acc_tags$pit[which(rowSums(acc_tags[,-1])>0)]
 
+   
 
 ## SET UP CAPTURE HISTORY FOR ALL FISH
 ch<-reshape2::dcast(xx,pit~secid,value.var="tmp",sum,
@@ -90,6 +81,16 @@ ch_acc<- ch[which(ch$pit%in% acc_tags),-1]
     
     
     
+   
+   
+## MERGE SECONDARY ID
+xx<-merge(taggingData,dat$secondary, by=c("occasionId","set_number"),all.x=TRUE)
+xx$tmp<-1
+xx$secid<-factor(xx$id.y,levels=dat$secondary$id)
+
+
+
+
    
 # MAKE A MATRIX OF WHEN FISH ARE AVAILABLE
 left<- data.frame(
@@ -107,11 +108,11 @@ tags_ac<- merge(tags,left,by="pit",all.x=TRUE)
 
 poolReciever<- sqlFetch(comm,"qry-pool-reciever-data")
 
-recieverData<-sqlQuery(comm,"SELECT [Reciever Data].[Date and Time (UTC)] AS [date], 
+recieverData<-sqlQuery(comm,'SELECT [Reciever Data].[Date and Time (UTC)] AS [date], 
     DatePart("yyyy",[Date and Time (UTC)]) AS [year], 
     DatePart("y",[Date and Time (UTC)]) AS doy, Right([Reciever Data.Transmitter],5) AS transmitter, [Reciever Data].[Site ID] AS siteId
     FROM [Reciever Data]
-    WHERE ((([Reciever Data].[Site ID])='12'));")
+    WHERE ((([Reciever Data].[Site ID])="12"));')
 
 
 # DATA AUGMENTATION
--- _R/7_analysis-m0.R
+++ _R/7_analysis-m0.R
@@ -1,3 +1,49 @@
+
+
+## GO BACK TO M0.
+makeData_MO<- function(occasion,...)
+    {
+    ## FISH ON RECIEVER
+    onReciever<- pp[,c(1,which(names(pp)==occasion))]
+    names(onReciever)<-c("pit","detected")
+    onReciever<- subset(onReciever,detected==1)
+
+    sets<- subset(ggg,id==occasion&set>0)
+    sets<-as.character(paste(sets$year,sets$doy,sets$set,sep="_"))
+    ch_dat<- subset(xxx,id==occasion & set>0)
+    ch_dat$occ_id<- factor(ch_dat$occ_id,
+        levels=sets)
+    if(nrow(ch_dat)>0 & !is.null(ncol(onReciever)))
+        {
+
+        ch<- dcast(ch_dat, pit~occ_id,
+            value.var='tmp',
+            drop=FALSE,
+            fun=sum)
+        ch<-merge(onReciever,ch,by="pit",all=TRUE)    
+        ch$detected<-ifelse(is.na(ch$detected==1),2,ch$detected)  
+        ch[is.na(ch)]<-0  
+        ch<-as.matrix(ch)
+        
+        cha<-ch[ch[,2]==1,-c(1:2)]
+        #chp<-as.matrix(ch[ch[,2]==2,-c(1:2)],nrow=length(which(ch$detected==2)) ,ncol=ncol(cha))
+        chp<-ch[ch[,2]==2,-c(1:2),drop=FALSE]
+        dat_aug<- 100
+        z<- c(rep(1,nrow(chp)),rep(0,dat_aug-nrow(chp)))
+        chp<-rbind(chp,matrix(0,nrow=dat_aug-nrow(chp) ,ncol(chp)))# data augmentation needs to be matrix of 0s not NAs for JAGs
+        out<- list(ch=as.matrix(chp),
+            cha=as.matrix(cha), 
+            Ncha=nrow(cha),
+            occ=ncol(chp),
+            M=nrow(chp))
+        
+        return(out)
+        }else{return(list(ret=-1))}
+    }
+    
+
+
+
 #######################################################################
 #
 #  M0 MODEL
@@ -41,3 +87,6 @@ presPlot()
 plot(NN~year_f,out,ylim=c(0,125),type='h',
     ylab="Abundance",xlab="Year",lwd=5)
 
+    
+    
+makeData_MO(40)
